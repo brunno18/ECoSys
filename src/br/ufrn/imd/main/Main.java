f95c19955f91421f95ab7078b8d0bce51ef2325a
Rubens Araújo <rubensaviana@hotmail.com>
2016-05-10 00:24:02 -0300

    Adicao da criacao do evento

--- src/br/ufrn/imd/main/Main.java
+++ src/br/ufrn/imd/main/Main.java
@@ -1,7 +1,42 @@
 package br.ufrn.imd.main;
+import java.util.*;
+
+import br.ufrn.imd.controller.*;
+import br.ufrn.imd.model.*;
 
 public class Main {
+	
+	
+	
+	@SuppressWarnings("resource")
 	public static void main (String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		GerenciadorEvento gerenciaEvento = new GerenciadorEvento();
+		
+		String titulo;
+		String desc;
+		String locale;
+		String participantes;
+		
+		
 		System.out.println("OO Framework to Event Control Systems");
+	
+		
+		System.out.println("Criar evento ");
+		
+		System.out.print("titulo: ");
+		titulo = in.nextLine();
+		System.out.print("descricao: ");
+		desc = in.nextLine();
+		System.out.print("local: ");
+		locale = in.nextLine();
+		System.out.print("participante: ");
+		participantes = in.nextLine();
+		
+		Evento evento = new Evento();
+		//gerenciaEvento.criarEvento(locale, participantes, desc, titulo);
+		
+		
 	}
 }
--- src/br/ufrn/imd/model/Evento.java
+++ src/br/ufrn/imd/model/Evento.java
@@ -1,8 +1,44 @@
 package br.ufrn.imd.model;
 
 public class Evento {
+	Local local;
+	Participante participante;
+	//Publicacao publicacao;
 	
 	private String title;
 	
 	private String descricao;
+	
+	public void setTitulo(String title){
+		this.title = title;
+	}
+	
+	public void setDescricao(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public void setLocal(Local local){
+		this.local = local;
+	}
+
+	public void setParticipante(Participante participante){
+		this.participante = participante;
+	}
+
+	public Local getLocal(){
+		return local;
+	}
+	
+	public Participante getParticipante(){
+		return participante;
+	}
+	
+	public String getTitulo(){
+		return title;
+	}
+	
+	public String getDescicao(){
+		return descricao;
+	}
 }
+
