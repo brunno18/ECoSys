a6e4c35d571a37032dc72e851e70cf7fffcb9522
Rubens Araújo <rubensaviana@hotmail.com>
2016-05-23 13:38:06 -0300

    Adição da Fabrica de Notificações e da classe Main

--- src/br/ufrn/imd/instanciaClinica/MainClinica.java
+++ src/br/ufrn/imd/instanciaClinica/MainClinica.java
@@ -1,11 +1,22 @@
 package br.ufrn.imd.instanciaClinica;
 
-import java.util.*;
+import java.text.ParseException;
+//import java.util.*;
+
+import br.ufrn.imd.model.ValidatePartipationException;
 
 public class MainClinica {
-	private static Scanner input = new Scanner(System.in);
+	//private static Scanner input = new Scanner(System.in);
 	
-	public static void main(String [] args){
+	private static UIGerenciaClinica manager = new UIGerenciaClinica();
+	
+	public static void main(String [] args) throws ParseException, ValidatePartipationException{
+		
+		manager.criarEvento();
+		//manager.atualizarEvento();
+		//manager.cancelarEvento();
+		manager.inscreverParticipante(1);
+		//manager.notificarEventosProximos();
 		
 	}
 }
